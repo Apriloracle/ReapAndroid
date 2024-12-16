be3c227d71afc192189fd0cf21ff74f13fcfbe50
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-12-16 14:09:32 -0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,15 @@ class MyDocument extends Document {
         <Head>
           <script src="https://unpkg.com/ml5@0.12.2/dist/ml5.min.js" defer></script>
           <link rel="manifest" href="/manifest.json" />
+          <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RNVC517N8T"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RNVC517N8T');
+</script>
         </Head>
         <body>
           <Main />
